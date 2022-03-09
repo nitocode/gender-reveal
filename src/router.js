ef1b90c77621f82ab627f4b7414c6d41a6661cbb
nitocode <lu.nitoumbi@gmail.com>
2022-03-09 18:52:50 +0100

    Add: Custom version and build

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue"
 import Setup from "./views/Setup.vue"
 import Reveal from "./views/Reveal.vue"
+import EstelleLoic from "./views/EstelleLoic.vue"
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +10,7 @@ export const router = createRouter({
       { name: 'home', path: '/', component: Home },
       { name: 'setup', path: '/setup', component: Setup },
       { name: 'reveal', path: '/reveal', component: Reveal },
+      { name: 'estelleloic', path: '/estelleloic', component: EstelleLoic },
     ],
   })
   
